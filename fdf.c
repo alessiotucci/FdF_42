8de339ed674967bf7c29bbe917f10c468bb21f99
Alessio Tucci <atucci@e3r10p7.42roma.it>
2023-06-28 10:16:32 +0200

    the makefile  doesnt work

--- fdf.c
+++ fdf.c
@@ -6,19 +6,32 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 13:33:06 by atucci            #+#    #+#             */
-/*   Updated: 2023/06/27 16:42:47 by atucci           ###   ########.fr       */
+/*   Updated: 2023/06/28 10:04:39 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <mlx.h>
+
+static	void	open_windows()
+{
+	void	*windows_pointer;
+	void	*pointer_needed;
+	
+	pointer_needed = mlx_init();
+	
+	windows_pointer = mlx_new_window(pointer_needed, 500, 500, "this is a try");
+
+	mlx_loop(pointer_needed);
+}
 
 int	main(int ac, char *av[])
 {
-	if (ac != 2)
+	if (ac != 2 && av)
 	{
 		ft_printf("exit\n");
 		exit(0);
 	}
 	else
-		return(ft_printf(av[1]));
+		open_windows();
 }
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 13:33:30 by atucci            #+#    #+#             */
-/*   Updated: 2023/06/27 16:34:31 by atucci           ###   ########.fr       */
+/*   Updated: 2023/06/27 16:59:08 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,18 @@
 
 // includes
 # include "libft/libft.h"
+// including minilibx library
+# include "minilibx_macos/mlx.h"
 # include <unistd.h>
 # include <limits.h>
 # include <stdlib.h>
 # include <stddef.h>
+// define  the default windows
+# ifndef DEFAULT_WIDTH
+#  define DEFAULT_WIDTH 1280
+# endif
 
+# ifndef DEFAULT_HEIGHT
+#  define DEFAULT_HEIGHT 720
+# endif
 #endif
