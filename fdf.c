7c4b565ddecabdd04762be661d37043d1df62e58
Alessio Tucci <atucci@e3r10p7.42roma.it>
2023-06-28 12:19:16 +0200

    checked the extension with strncmp

--- fdf.c
+++ fdf.c
@@ -6,17 +6,33 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 13:33:06 by atucci            #+#    #+#             */
-/*   Updated: 2023/06/28 11:32:12 by atucci           ###   ########.fr       */
+/*   Updated: 2023/06/28 12:16:48 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <mlx.h>
+
+int	check_the_map(char *file_name)
+{
+	int len;
+	char *last_char;
+	char compare[] = ".fdf";
+	int result;
+	
+	len = ft_strlen(file_name);
+	last_char = &file_name[len - 4];
+	result = ft_strncmp(last_char, compare, len);
+	if (result != 0)
+    return(ft_printf("\033[1;37;45mWrong file name\n"));
+	return (result);
+
+}
+
  void draw_a_point(void *ptr_need, void *windows_ptr, int x, int y, int color)
 {
-
-mlx_pixel_put(ptr_need, windows_ptr,  x, y, color);
-return ;
+	mlx_pixel_put(ptr_need, windows_ptr,  x, y, color);
+	return ;
 }
 static	void	open_windows(int wid, int hei)
 {
@@ -39,14 +55,20 @@ int	main(int ac, char *av[])
 {
 	int width;
 	int	height;
+	int	fd;
 	
 	width = STANDARD_WINDOWS_WIDTH;
 	height = STANDARD_WINDOWS_HEIGHT;
+	fd = ft_printf("\033[1;34mI am reading this file...\033[1;34m\n");   // Set text color to blue
+
+	
 	if (ac != 2 && av)
 	{
-		ft_printf("exit\n");
+		ft_printf("\033[1;31mexit\n");
 		exit(0);
 	}
-	else
-		open_windows(width, height);
+	// I need to perfom the check of the map
+	fd = check_the_map(av[1]);
+	if (fd == 0)
+	open_windows(width, height);
 }
