54575a9598e9dd51df2aef240854cced72b59ce8
Alessio Tucci <atucci@e3r10p7.42roma.it>
2023-06-28 11:37:38 +0200

    modify

--- fdf.c
+++ fdf.c
@@ -6,32 +6,47 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 13:33:06 by atucci            #+#    #+#             */
-/*   Updated: 2023/06/28 10:04:39 by atucci           ###   ########.fr       */
+/*   Updated: 2023/06/28 11:32:12 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <mlx.h>
+ void draw_a_point(void *ptr_need, void *windows_ptr, int x, int y, int color)
+{
 
-static	void	open_windows()
+mlx_pixel_put(ptr_need, windows_ptr,  x, y, color);
+return ;
+}
+static	void	open_windows(int wid, int hei)
 {
 	void	*windows_pointer;
 	void	*pointer_needed;
 	
 	pointer_needed = mlx_init();
+	// adding this check
+	if (!pointer_needed)
+		exit(1);
 	
-	windows_pointer = mlx_new_window(pointer_needed, 500, 500, "this is a try");
+	windows_pointer = mlx_new_window(pointer_needed, wid, hei, "this is a try");
+
+	draw_a_point(pointer_needed, windows_pointer, 250, 250, 0xFFFFFF);
 
 	mlx_loop(pointer_needed);
 }
 
 int	main(int ac, char *av[])
 {
+	int width;
+	int	height;
+	
+	width = STANDARD_WINDOWS_WIDTH;
+	height = STANDARD_WINDOWS_HEIGHT;
 	if (ac != 2 && av)
 	{
 		ft_printf("exit\n");
 		exit(0);
 	}
 	else
-		open_windows();
+		open_windows(width, height);
 }
--- fdf.h
+++ fdf.h
@@ -6,13 +6,21 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 13:33:30 by atucci            #+#    #+#             */
-/*   Updated: 2023/06/27 16:59:08 by atucci           ###   ########.fr       */
+/*   Updated: 2023/06/28 11:23:24 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef FDF_H
 # define FDF_H
+// define the standard windows size
+# ifndef STANDARD_WINDOWS_WIDTH
+#  define STANDARD_WINDOWS_WIDTH 640
+# endif
+
+# ifndef STANDARD_WINDOWS_HEIGTH
+#  define STANDARD_WINDOWS_HEIGHT 480
+# endif
 
 // includes
 # include "libft/libft.h"
@@ -22,12 +30,5 @@
 # include <limits.h>
 # include <stdlib.h>
 # include <stddef.h>
-// define  the default windows
-# ifndef DEFAULT_WIDTH
-#  define DEFAULT_WIDTH 1280
-# endif
 
-# ifndef DEFAULT_HEIGHT
-#  define DEFAULT_HEIGHT 720
-# endif
 #endif
