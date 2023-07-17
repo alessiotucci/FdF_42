2500b4cb57f5b374caf6118298daa70727b2fbd1
Alessio Tucci <alessiotucci33@gmail.com>
2023-07-17 11:10:45 +0200

    prova

--- windows_draws.c
+++ windows_draws.c
@@ -6,7 +6,7 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 16:19:48 by atucci            #+#    #+#             */
-/*   Updated: 2023/07/13 15:09:33 by atucci           ###   ########.fr       */
+/*   Updated: 2023/07/17 11:08:29 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,9 @@ void	line_between_points(t_line **mappa, void *ptr_need, void *windows_ptr)
 }
 
 
+
+
+
 void    open_windows(int wid, int hei, t_line **mappa)
 {
 	void	*windows_pointer;
@@ -85,7 +88,8 @@ void    open_windows(int wid, int hei, t_line **mappa)
 	// I need to extract this while loops to create an other function to draw lines
 	while (tmp != NULL)
  	{
-	line_between_points(mappa, pointer_needed, windows_pointer);	
+	line_between_points(mappa, pointer_needed, windows_pointer);
+	
 //	pixel_x = (tmp->x * 640) / 3;// this is a small add, the number 3 is bc the map I am testing is a 3X3
 //	pixel_y = (tmp->y * 480) / 3;// in other case it should be the widht and height of the map, leave it like this for now
 	draw_a_point(pointer_needed, windows_pointer, tmp->x, tmp->y, 0xFFFFFF);
