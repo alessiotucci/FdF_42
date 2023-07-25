1d571e8a5a4ea1a7ff9e543d5d5f78abafc35671
Alessio Tucci <alessiotucci33@gmail.com>
2023-07-25 16:30:11 +0200

    added the license for isometric function

--- new_version/fdf_isometric_bonus.c
+++ new_version/fdf_isometric_bonus.c
@@ -6,9 +6,30 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 14:15:28 by atucci            #+#    #+#             */
-/*   Updated: 2023/07/25 16:26:04 by atucci           ###   ########.fr       */
+/*   Updated: 2023/07/25 16:29:54 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/* 
+* This software is made available to anyone who wants to retrace the 
+* author's learning path through the projects of school 42.
+* Copyright (C) 2023  fporciel
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+* You can contact the author at: 
+*- fporciel@student.42roma.it
+*/
 
 #include "fdf.h"
 /* build a macro to work  with radiants */
