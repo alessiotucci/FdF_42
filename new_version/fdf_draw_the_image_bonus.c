bf43884b2d729fb83955123250358c30d4206ae5
Alessio Tucci <alessiotucci33@gmail.com>
2023-07-26 18:10:21 +0200

    I starting to process the huge amount of information, maybe  il write decent code

--- new_version/fdf_draw_the_image_bonus.c
+++ new_version/fdf_draw_the_image_bonus.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 14:19:26 by atucci            #+#    #+#             */
-/*   Updated: 2023/07/26 17:59:51 by atucci           ###   ########.fr       */
+/*   Updated: 2023/07/26 18:09:39 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,34 @@
 // color the point of  the maps in the image
 
 // bresenham algorithm function
+static  void light_a_pixel(t_map *point)
+{
+	int	i;
+
+	// i is gonna be the INDEX;
+}
+
 
 int	draw_lines(t_data *info, t_map ***map)
 {
-// this is gonna be the start of the program
-
-// general formula to find the index in the char * (a string)
-// index = lsize * y + x * (bits / 8)
-//	int	i;
-//	i = info->lsize * y + x * (info->bits / 8); 
+// I have to set up a while cycle to go throught all the point of the map!
+	int	y;
+	int	x;
+	
+	y = 0;
+	while (y <= info->max_y)
+	{
+		x = 0;
+		while (x <= info->max_x)
+		{
+		ligh_a_pixel(&(*info)[y][x]);
+		x++;
+		}
+	y++;
+	}
+	return (0);
 }
+/* general formula to find the index in the char * (a string)
+ index = lsize * y + x * (bits / 8)
+	int	i;
+	i = info->lsize * y + x * (info->bits / 8);*/ 
