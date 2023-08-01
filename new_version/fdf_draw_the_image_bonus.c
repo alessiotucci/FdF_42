3a61227f0163c2a997974869345c2f73c391c385
Alessio Tucci <alessiotucci33@gmail.com>
2023-08-01 12:08:25 +0200

    re-wrote entirely the bresenham algorithm, norm-friendly, without using dumb struct

--- new_version/fdf_draw_the_image_bonus.c
+++ new_version/fdf_draw_the_image_bonus.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 14:19:26 by atucci            #+#    #+#             */
-/*   Updated: 2023/08/01 10:18:36 by atucci           ###   ########.fr       */
+/*   Updated: 2023/08/01 12:06:20 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,93 +26,67 @@ void	printMap(t_map *map)
 	ft_printf("\033[1;47mright_point: %p\033[0m\n",	(void*)map->right_point);
 	ft_printf("\033[1;47mdown_point: %p\033[0m\n",	(void*)map->down_point);
 }
-//*/
-/* Initialize the Bresenham parameters for the current line*/
-void bresenham_init(t_bres *params, t_map *start, t_map *end)
-{
-	ft_printf("FUNCTION CALL: bresenhham_init\n");
-	params->x0 = start->x_display;
-	params->y0 = start->y_display;
-	params->delta_x = abs(end->x_display - params->x0);
-	params->delta_y = abs(end->y_display - params->y0);
-	params->slope = params->delta_y - params->delta_x;
-	params->x = params->x0;
-	params->y = params->y0;
-	//ft_printf("I am  a dumb  fuck, params->y[%d]\n", params->y);
-}
-
-/*/ Calculate the next point on the line using the Bresenham algorithm
-// Return 1 if there are more points to draw, 0 if we reached the end point*/
-static int bresenham_next(t_bres *params, t_map *start)
-{
-	static int check_if_first;
-	
-	check_if_first = 0;
-//	Check if we reached the end point
-	if ((params->x != start->x_display) || (check_if_first == 0))
-	{
-		if (params->slope > 0)
-		{
-			if (params->y > params->y0) // Slope is greater than 1
-				params->y += 1;
-			else
-				params->y -= 1;
-			params->slope -= params->delta_x;
-		}
-		else
-			params->slope += params->delta_y; // Slope is less than or equal to 1
-		// Move to the next pixel
-		if (params->x > start->x_display)
-			params->x += 1;
-		else
-			params->x -= 1;
-		check_if_first++;
-		return (1);// Return 1 to indicate there are more points to draw
-	}
-	// Return 0 to indicate we reached the end point
-	ft_printf("Return 0 to indicate we reached the end point\nvalue of int[%d]\n", check_if_first);
-	return (0);
-}
-
 /* Color the point of the maps in the image*/
-static void my_put_pixel(t_data *info, t_bres *param)
+static void my_put_pixel(t_data *info, int x, int y)
 {
-	int	pixel_x;	
-	int	pixel_y;
+//	int	pixel_x;
+//	int	pixel_y;
 	int	index;
 
-	ft_printf("\033[1;45mIssue of segvault\033[0m\n");
-	if (info == NULL || param == NULL)
+	ft_printf("\033[1;45mbits lsize are wrong\033[0m\n");
+	if (info == NULL)
 		return; // Do nothing if the parameters are NULL
-	pixel_x = param->x;
-	pixel_y = param->y;
-	ft_printf("\033[1;36minfo->lsize[%d]\033[0m\n", info->lsize);
-	ft_printf("\033[1;36minfo->bits[%d]\033[0m\n", (info->bits));
-	ft_printf("\033[1;36mpixel_y: %d, pixel_x: %d\033[0m\n", pixel_x, pixel_y);
-	index = (size_t)info->lsize * pixel_y + pixel_x * ((size_t)info->bits / 8);
-	ft_printf("\033[1;46mthe value of  index [%d]\033[0m\n", index);
+//	pixel_x = param->x;
+//	pixel_y = param->y;
+//	ft_printf("\033[1;36minfo->lsize[%d]\033[0m\n", info->lsize);
+//	ft_printf("\033[1;36minfo->bits[%d]\033[0m\n", (info->bits));
+	ft_printf("\033[1;36mpixel_y: %d, pixel_x: %d\033[0m\n", x, y);
+	index = (size_t)info->lsize * y + x * ((size_t)info->bits / 8);
+	ft_printf("\033[1;46mthe value of index [%d]\033[0m\n", index);
 	ft_printf("\033[1;45mThe projection type  is {%c}\033[0m\n", info->projection_type);
 	ft_printf("this is the len of img_data: %d\n", ft_strlen(info->img_data));
 	ft_printf("\033[1;45mThe img_data pointer is %s \033[0m\n", info->img_data);
 	//info->img_data[index] = 255; // Set the pixel to red
-	if (info->img_data == NULL)
-		exit(0);
+		//exit(0);
 	return ;
 }
 
-/* Draw a line between two points on the map using the Bresenham algorithm*/
-static void drawing(t_data *info, t_map *start, t_map *end)
+static void	bresenham(t_data *info, t_map *s, t_map *e)
 {
-	static  int count = 0;
-	ft_printf("printing  the start[%d]\n", count);
-	printMap(start);
-	static t_bres param;
-	bresenham_init(&param, start, end);
-	while (bresenham_next(&param, start) && (count < 10)) // this has been modified for testing
+	int	delta_x;
+	int	delta_y;
+	int	p;
+	int	x;
+	int	y;
+
+	delta_x = e->x_display - s->x_display;
+	delta_y = e->y_display - s->y_display;
+	x = s->x_display;
+	y = s->y_display;
+	p = 2 * delta_y - delta_x;
+	while (x < e->x_display)
 	{
-		my_put_pixel(info, &param);
-		count++;
+		my_put_pixel(info, x, y);
+		if (p >= 0)
+		{
+			y = y + 1;
+			p = p + 2 * delta_y - 2 * delta_x;
+		}
+		else
+			p = p + 2 * delta_y;
+		x = x + 1;
 	}
+}
+
+
+/* Draw a line between two points on the map using the Bresenham algorithm*/
+static void drawing(t_data *info, t_map *str, t_map *end)
+{
+//	int count = 0;
+	ft_printf("inside the drawing\tfunction\n");
+	printMap(str);
+// let rewrite the bresenham algorithm;
+	bresenham(info, str, end);
 }
 
 /*Function to draw lines between all points on the map*/
