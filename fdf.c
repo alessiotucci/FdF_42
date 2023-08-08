cb12204e6c8eb7af173fc11988908e2b53edad41
Alessio Tucci <alessiotucci33@gmail.com>
2023-08-08 11:17:58 +0200

    renaming and organizing  all

---
+++ fdf.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 10:30:57 by atucci            #+#    #+#             */
+/*   Updated: 2023/08/08 11:13:57 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static int	check_the_map(char *file_name, t_data info)
+{
+	int			fd;
+	char		*helping;
+	char		**matrix;
+	int			count_y;
+	int			count_x;
+
+	count_x = 0;
+	count_y = 0;
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf("\033[1;31mCan't read the map 😴 \033[0m\n");
+		return (fd);
+	}
+	while ((helping = get_next_line(fd)) != NULL)
+	{
+		if (!helping)
+			return (-1);
+		matrix = ft_split(helping, ' ');
+		while (matrix[count_y] != NULL)
+		{
+			count_x = 0;
+			while (matrix[count_y][count_x] != '\0' && matrix[count_y][count_x] != '\n')
+				count_x++;
+		}
+		count_y++;
+	}
+	info.max_x = count_x;
+	info.max_y = count_y;
+	ft_printf("map max x %d\n", info.max_x);
+	ft_printf("map max y %d\n", info.max_y);
+	return (fd);
+}
+
+static int	check_windows_size(char *width, char *height)
+{
+	size_t	count;
+
+	count = 0;
+	while (width[count])
+	{
+		if ((width[count] < 48) || (width[count] > 57))
+		{
+			ft_printf("\033[1;31mwrong size\033[0m\n");
+			return (0);
+		}
+		count++;
+	}
+	count = 0;
+	while (height[count])
+	{
+		if ((height[count] < 48) || (height[count] > 57))
+		{
+			ft_printf("\033[1;31mwrong size \033[0m\n");
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+static int	check_the_extension(char *name_of_map)
+{
+	int	len;
+
+	len = strlen(name_of_map);
+	if (len < 4)
+	{
+		ft_printf("\033[1;41mmap name is very short!\033[0m\n");
+		return (0);
+	}
+	if (strncmp(name_of_map + len - 4, ".fdf", 4) == 0)
+	{
+		ft_printf("\033[1;32mGood file extension👍 \033[0m\n");
+		return (1);
+	}
+	ft_printf("\033[1;41mNot a valid .fdf extension\033[0m\n");
+	return (0);
+}
+
+int	main(int ac, char *av[])
+{
+	static t_data	info;
+
+	info.win_width = DEFAULT_WIDTH;
+	info.win_height = DEFAULT_HEIGHT;
+	info.fd = ft_printf("let me check the params\n");
+	if (ac < 2)
+		return (ft_printf("Not  enough params\n"));
+	if ((ac >= 2) && (ac <= 5))
+	{
+		info.fd = check_the_extension(av[1]);
+		if (info.fd == 0)
+			return (0);
+		if (ac >= 4)
+		{
+			if (check_windows_size(av[2], av[3]))
+			{
+				info.win_width = ft_atoi(av[2]);
+				info.win_height = ft_atoi(av[3]);
+			}
+			info.fd = check_the_map(av[1], info);
+		}
+		ft_printf("finestra opt%d\n", info.win_width);
+		ft_printf("finestra opt%d\n", info.win_height);
+		ft_printf("fd:%d\n", info.fd);
+		if (info.fd >= 0)
+			map_registration(&info);
+		return (0);
+	}
+}
+
--- fdf.h
+++ fdf.h
@@ -3,13 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
+/*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/01 13:33:30 by atucci            #+#    #+#             */
-/*   Updated: 2023/07/19 14:21:59 by atucci           ###   ########.fr       */
+/*   Created: 2023/07/10 10:35:56 by atucci            #+#    #+#             */
+/*   Updated: 2023/08/08 11:15:40 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef FDF_H
 # define FDF_H
@@ -17,16 +16,6 @@
 # include "./mlx_linux/mlx.h"
 # include "./mlx_linux/mlx_int.h"
 # include "./libft/libft.h"
- 
-# ifndef DEFAULT_WIDTH
-# define DEFAULT_WIDTH 1280
-# endif 
-
-# ifndef DEFAULT_HEIGHT
-# define DEFAULT_HEIGHT 720
-# endif 
-
-
 # include <unistd.h>
 # include <string.h>
 # include <stdio.h>
@@ -35,6 +24,14 @@
 # include <errno.h>
 # include <fcntl.h>
 # include <sys/stat.h>
+
+# ifndef DEFAULT_WIDTH
+# define DEFAULT_WIDTH 1280
+# endif 
+
+# ifndef DEFAULT_HEIGHT
+# define DEFAULT_HEIGHT 720
+# endif 
 
 typedef struct s_data
 {
@@ -50,30 +47,39 @@ typedef struct s_data
 	int			chg_projection;
 	int			max_x;
 	int			max_y;
+	int			y_count;
+	int			x_count;
+	int			z_count;
 	ssize_t		count;
 	char		projection_type;
+	char		pre_buf;
+	char		nex_buf;
 }				t_data;
 
 typedef struct s_map
 {
-	int	x_orthogonal;
-	int	y_orthogonal;
-	int	z_orthogonal;
-	int	x_display;
-	int	y_display;
-	int	color;
-	struct  s_map	*right_point;
-	struct  s_map	*down_point;
+	int					x_orthogonal;
+	int					y_orthogonal;
+	int					z_orthogonal;
+	int					x_display;
+	int					y_display;
+	int					z_display;
+	int					color;
+	int					stop_param;
+	struct s__map		*right_point;
+	struct s_map		*down_point;
+}						t_map;
 
-}		t_map;
 
 int	main(int ac, char *av[]);
 int	map_registration(t_data *info);
-int	points_conjuction(t_data *info, t_map ***map);
+int	points_conjunction(t_data *info, t_map ***map);
 int	connection_management(t_data *info, t_map ***map);
 int	isometric(t_data *info, t_map ***map);
 int	image_management(t_data *info, t_map ***map);
 int	set_pixel_values(t_data *info, t_map ***map);
 int	display_image(t_data *info, t_map ***map);
-
+// to test 
+void printMap( t_map *map);
 #endif
+
