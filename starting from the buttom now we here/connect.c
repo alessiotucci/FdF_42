3b77d555c585eab689f5356c58d873a009f1950e
Alessio Tucci <alessiotucci33@gmail.com>
2023-09-21 09:15:29 +0200

    never back down never WHAT

---
+++ starting from the buttom now we here/connect.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   connect.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 11:21:47 by atucci            #+#    #+#             */
+/*   Updated: 2023/09/21 08:58:57 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static	void update_point_connect(t_point *curr, t_point *right, t_point *down)
+{
+	if (right != NULL)
+		curr->go_right = right;
+	if (down != NULL)
+		curr->go_down = down;
+}
+
+/*Function to update the pointers of the linked list*/
+int	point_connect(t_point **head, t_date *my_info)
+{
+	t_point *current;
+	int	y;
+	int	x;
+
+	y = 0;
+	current = *head;
+	while (y < my_info->colums) // double check this while condititon
+	{
+		x = 0;
+		while (x < my_info->rows)
+		{
+			if (current->next->x_map == current->x_map)
+				update_point_connect(current, current->next, NULL);
+		// update the curr = curr->next;
+		x++;
+		}
+		y++;
+	}
+return (0);
+}
