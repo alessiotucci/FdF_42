eff8a687176ea1bb482e966c2783ff532e6722e5
Alessio Tucci <alessiotucci33@gmail.com>
2023-10-01 14:00:56 +0200

    bresenham has issue and it is too long for the norm

--- starting from the buttom now we here/bresenham.c
+++ starting from the buttom now we here/bresenham.c
@@ -6,7 +6,7 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 18:34:25 by atucci            #+#    #+#             */
-/*   Updated: 2023/10/01 11:43:01 by atucci           ###   ########.fr       */
+/*   Updated: 2023/10/01 13:59:34 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,16 @@ void	bresenham(t_date *info, t_point *start, t_point *end)
 	int	x;
 	int	y;
 
-
 	delta_x = end->x_pixel - start->x_pixel;
 	delta_y = end->y_pixel - start->y_pixel;
-
 	x = start->x_pixel;
 	y = start->y_pixel;
 	p = 2 * delta_y - delta_x;
 	if (delta_x == 0)
-			while (y <= end->y_pixel)
+		while (y <= end->y_pixel)
 			draw_point(info, x, y++);
 	while (x <= end->x_pixel)
 	{
-		// do some drawing
 		draw_point(info, x, y);
 		if (p >= 0)
 		{
@@ -46,32 +43,18 @@ void	bresenham(t_date *info, t_point *start, t_point *end)
 }
 
 /* In this function we will try to set pixel in the img of mlx*/
-void draw_point(t_date *info, int x, int y)
+void	draw_point(t_date *info, int x, int y)
 {
-	//ft_printf("\t[%d][%d]\t\n", x, y);
-	
+	ft_printf("\t[%d][%d]\t\n", x, y);
 	if (x >= 0 && x < DEFAULT_WIDTH && y >= 0 && y < DEFAULT_HEIGHT)
 		my_mlx_pixel_put(info, x, y, COLOR_WHITE);
-/*
-	int	index;
-// bits and lsize are inside graphics...
-	if (info == NULL)
-		return; // Do nothing if the parameters are NULL
-	//if (x >= 0 && x < info->width && y >= 0 && y < info->height)
-	//{
-	index = (info->lsize * y) + (x * (info->bits / 8));
-	info->img_string[index] = (unsigned char)255; // Set the pixel to red
-	// idk what is this
-info->img_string[index + 1] = (unsigned char)255; // Green channel
-info->img_string[index + 2] = (unsigned char)255; // Blue channel
-	//}*/
 	return ;
 }
 
-/* In this function we will loop throught the list of points and draw using the bresenham funciton*/
+/* In this function we will loop throught the list 
+	* of points and draw using the bresenham funciton*/
 void	draw_lines(t_date *info, t_point **head)
 {
-	ft_printf("\n--\t---\n[3]: check draw lines--> call bresenham --> call draw point\n");
 	t_point	*current;
 
 	current = *head;
@@ -79,12 +62,12 @@ void	draw_lines(t_date *info, t_point **head)
 	{
 		if (current->go_right != NULL)
 		{
-		ft_printf("%s \t- BRESENHAM[%p] going right %s\t-->\n", BG_CYAN,current, RESET);
+			ft_printf("%sbres:[%p]going right%s-->\n", BG_CYAN, current, RESET);
 			bresenham(info, current, current->go_right);
 		}
-			if (current->go_down != NULL)
+		if (current->go_down != NULL)
 		{
-		ft_printf("%s \t- BRESENHAM[%p] going down %s\t--v\n", BG_CYAN,current, RESET);
+			ft_printf("%sbres:[%p]going down%s--v\n", BG_CYAN, current, RESET);
 			bresenham(info, current, current->go_down);
 		}
 		current = current->next;
